61e1a66e04901759cd323f1f0648f43be1d77526
caitlin <caitlinlopez@users.noreply.github.com>
2016-10-21 11:08:27 -0700

    Shift cipher decoder.

--- encoder.js
+++ encoder.js
@@ -30,6 +30,36 @@ var letters = {
   90 : "z"
 }
 
+var decoder = {
+  65 : "z",
+  66 : "y",
+  67 : "x",
+  68 : "w",
+  69 : "v",
+  70 : "u",
+  71 : "t",
+  72 : "s",
+  73 : "r",
+  74 : "q",
+  75 : "p",
+  76 : "o",
+  77 : "n",
+  78 : "m",
+  79 : "l",
+  80 : "k",
+  81 : "j",
+  82 : "i",
+  83 : "h",
+  84 : "g",
+  85 : "f",
+  86 : "e",
+  87 : "d",
+  88 : "c",
+  89 : "b",
+  90 : "a"
+}
+
+//changing which radio button is checked
 $("input[type='radio']").click(function(){
    $("input:checked").prop('checked', false);
    $(this).prop('checked', true);
@@ -48,3 +78,34 @@ $("#inputText").keydown(function(e) {
     $("#textArea").append(letters[userKey]);
   }
 });
+
+
+$("#translationInputText").keydown(function(e) {
+  var userKey = e.keyCode;
+
+  //backspace
+  if (userKey == 8) {
+      translationArea.textContent = translationArea.textContent.substr(0, translationArea.textContent.length - 1);
+  }
+  //decoder
+  else {
+    $("#translationArea").append(decoder[userKey]);
+  }
+});
+
+/*
+decoder
+
+$("#translationInputText").keydown(function(e) {
+  if(e.keyCode == 13){
+
+    var message = $(#translationInputText).val();
+    var messageArray = message.split("");
+
+    for (var i = o; i < messageArraylength; i++){
+      var translateNumber = messageArray[i].charCodeAt(0)-32+1;
+    }
+  }
+});
+
+*/
